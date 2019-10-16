5533d63110e1071b815cb51682928b0b8f309d1c
WillJobe <william.jobe@gmail.com>
2019-10-16 17:05:12 +0200

    Create higher_order_functions.js

---
+++ higher_order_functions.js
@@ -0,0 +1,23 @@
+const f = (m) => console.log(m);
+f('Test');
+
+const obj = {
+    f(m) {
+      console.log(m)
+    }
+  }
+  obj.f('Test from object')
+
+const ff = (m) => () => console.log(m);
+const f2 = (f3) => f3();
+f2(ff('Test as argument'));
+
+function create_multiplier(x) {
+    function multiplier(y) {
+        return x * y;
+    }
+    return multiplier;
+}
+const multiplier_number_by_four = create_multiplier(4);
+multiplier_number_by_four(7);
+console.log(multiplier_number_by_four(7));
