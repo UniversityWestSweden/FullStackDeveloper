282cf53cd1fb3cd4ebef56750a75f288efc28df1
WillJobe <william.jobe@gmail.com>
2019-10-16 17:35:27 +0200

    Create anonymous_functions.js

---
+++ anonymous_functions.js
@@ -0,0 +1,14 @@
+function hello() {
+    console.log('Hello world');
+  }
+  hello();
+
+  var anon = function() {
+    console.log('I am anonymous');
+  }
+  anon();
+
+  var anon_arrow = () => {
+    console.log('I am an anonymous arrow funnction');
+  };
+  anon_arrow();
